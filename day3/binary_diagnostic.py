6c902aab59bcc1d698609bf6b4973fc3d8fc09e6
Pranas Ziaukas <pranas@balena.io>
2021-12-04 10:34:20 +0200

    solution: day3

--- day3/binary_diagnostic.py
+++ day3/binary_diagnostic.py
@@ -1,14 +1,44 @@
-from collections import defaultdict, Counter
+from typing import Callable, Tuple, Union
+
+Bit = str
+Bits = Union[Bit, Tuple[Bit]]
 
 INVERTER = {str(n): str(1 - n) for n in range(2)}
+
+
+def invert(bits: Bits) -> Bits:
+    return "".join(INVERTER[x] for x in bits)
+
+
+def most_popular_bit(bits: Bits) -> Bit:
+    return sorted(bits)[len(bits) // 2]
+
+
+def least_popular_bit(bits: Bits) -> Bit:
+    return invert(most_popular_bit(bits))
 
 
 class Report:
     def __init__(self, readings: list[str]) -> None:
         self.readings = readings
-        top_bits = [sorted(positions)[len(positions) // 2] for positions in zip(*readings)]
-        self.gamma_rate = int("".join(top_bits), 2)
-        self.epsilon_rate = int("".join(INVERTER[bit] for bit in top_bits), 2)
+
+        gamma_bits = "".join(most_popular_bit(bits) for bits in zip(*readings))
+        self.gamma_rate = int(gamma_bits, 2)
+        self.epsilon_rate = int(invert(gamma_bits), 2)
+
+        def find_reading(criteria: Callable[[Bits], Bit]) -> Bits:
+            current_readings = self.readings
+            position_bits = list(zip(*current_readings))
+            for n in range(len(position_bits)):
+                required_bit = criteria(position_bits[n])
+                current_readings = [reading for reading in current_readings if reading[n] == required_bit]
+                if len(current_readings) == 1:
+                    break
+                position_bits = list(zip(*current_readings))
+            return current_readings[0]
+
+        self.oxygen_generator_rating = int(find_reading(most_popular_bit), 2)
+        self.co2_scrubber_rating = int(find_reading(least_popular_bit), 2)
 
 
 if __name__ == "__main__":
@@ -19,3 +49,4 @@ if __name__ == "__main__":
 
     report = Report(data)
     puzzle.answer_a = report.gamma_rate * report.epsilon_rate
+    puzzle.answer_b = report.oxygen_generator_rating * report.co2_scrubber_rating
--- day3/binary_diagnostic_test.py
+++ day3/binary_diagnostic_test.py
@@ -5,7 +5,7 @@ from binary_diagnostic import Report
 
 class BinaryDiagnosticTest(unittest.TestCase):
     def setUp(self):
-        self.data = [
+        data = [
             "00100",
             "11110",
             "10110",
@@ -20,10 +20,15 @@ class BinaryDiagnosticTest(unittest.TestCase):
             "01010",
         ]
 
+        self.report = Report(data)
+
     def test_simple(self):
-        report = Report(self.data)
-        self.assertEqual(22, report.gamma_rate)
-        self.assertEqual(9, report.epsilon_rate)
+        self.assertEqual(22, self.report.gamma_rate)
+        self.assertEqual(9, self.report.epsilon_rate)
+
+    def test_advanced(self):
+        self.assertEqual(23, self.report.oxygen_generator_rating)
+        self.assertEqual(10, self.report.co2_scrubber_rating)
 
 
 if __name__ == "__main__":
