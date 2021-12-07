40a542296aff169d1fb89e679a20bb5956726132
Pranas Ziaukas <pranas@balena.io>
2021-12-07 12:39:18 +0200

    solution: day7

--- day07/crab_alignment.py
+++ day07/crab_alignment.py
@@ -1,20 +1,38 @@
+from typing import Callable
+import math
+
+
+def linear_cost(distance: int) -> int:
+    return distance
+
+
+def compounding_cost(distance: int) -> int:
+    return distance * (distance + 1) // 2
+
+
 class CrabSwarm:
     def __init__(self, positions: list[int]):
-        self.positions = positions
+        self._positions = sorted(positions)
 
-    def get_optimal_position(self) -> int:
-        return sorted(self.positions)[len(self.positions) // 2]
+        self.median = self._positions[len(self._positions) // 2]
+        self.average = sum(self._positions) / len(self._positions)
 
-    def get_fuel_consumption(self, position: int) -> int:
-        return sum(abs(position - x) for x in self.positions)
+    def get_fuel_consumption(self, position: int, cost_function: Callable[[int], int] = linear_cost) -> int:
+        return sum(cost_function(abs(position - x)) for x in self._positions)
 
 
 if __name__ == "__main__":
-    from aocd import models, transforms
+    from aocd import models
 
     puzzle = models.Puzzle(year=2021, day=7)
     data = [int(x) for x in puzzle.input_data.split(",")]
 
     swarm = CrabSwarm(data)
-    optimal_position = swarm.get_optimal_position()
-    puzzle.answer_a = swarm.get_fuel_consumption(optimal_position)
+    # Median minimizes /sum_{x} |x - x*|
+    optimal_position_linear = swarm.median
+    puzzle.answer_a = swarm.get_fuel_consumption(optimal_position_linear)
+
+    # Average approximately minimizes /sum_{x} |x - x*| (|x - x*| + 1) / 2
+    # over integer values
+    optimal_positions_compounding = [math.floor(swarm.average), math.ceil(swarm.average)]
+    puzzle.answer_b = min(swarm.get_fuel_consumption(p, compounding_cost) for p in optimal_positions_compounding)
--- day07/crab_alignment_test.py
+++ day07/crab_alignment_test.py
@@ -1,21 +1,29 @@
 import unittest
 
-from crab_alignment import CrabSwarm
+from crab_alignment import CrabSwarm, compounding_cost
+import math
 
 
-class FooTest(unittest.TestCase):
+class CrabAlignmentTest(unittest.TestCase):
     def setUp(self):
         data = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
         self.swarm = CrabSwarm(data)
 
-    def test_optimal_position(self):
-        self.assertEqual(2, self.swarm.get_optimal_position())
+    def test_median_position(self):
+        self.assertEqual(2, self.swarm.median)
 
     def test_fuel_consumptions(self):
         self.assertEqual(37, self.swarm.get_fuel_consumption(2))
         self.assertEqual(41, self.swarm.get_fuel_consumption(1))
         self.assertEqual(39, self.swarm.get_fuel_consumption(3))
         self.assertEqual(71, self.swarm.get_fuel_consumption(10))
+
+    def test_approx_average_position(self):
+        self.assertTrue(5 in [math.floor(self.swarm.average), math.ceil(self.swarm.average)])
+
+    def test_compounding_fuel_consumptions(self):
+        self.assertEqual(168, self.swarm.get_fuel_consumption(5, compounding_cost))
+        self.assertEqual(206, self.swarm.get_fuel_consumption(2, compounding_cost))
 
 
 if __name__ == "__main__":
