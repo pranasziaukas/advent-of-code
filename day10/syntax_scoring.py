bbc93afda6bbeabda306faad7075b39ca98484f7
Pranas Ziaukas <pranas@balena.io>
2021-12-11 19:37:13 +0200

    solution: day10

--- day10/syntax_scoring.py
+++ day10/syntax_scoring.py
@@ -1,29 +1,59 @@
 from collections import deque
+from dataclasses import dataclass
+from enum import Enum, auto
 
 
-def get_error_score(entry: str) -> int:
-    openers_closers = {
-        "(": ")",
-        "[": "]",
-        "{": "}",
-        "<": ">",
-    }
+class Type(Enum):
+    ERROR = auto()
+    INCOMPLETE = auto()
 
-    symbol_scores = {
+
+@dataclass
+class SyntaxScore:
+    type: Type
+    value: int
+
+
+OPENERS_CLOSERS = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+    "<": ">",
+}
+
+
+def get_score(entry: str) -> SyntaxScore:
+    """Get score of a known bad navigation subsystem entry."""
+    queue = deque()
+
+    # error checker
+    error_scores = {
         ")": 3,
         "]": 57,
         "}": 1197,
         ">": 25137,
     }
 
-    queue = deque()
     for symbol in entry:
-        if symbol in openers_closers.keys():
+        if symbol in OPENERS_CLOSERS.keys():
             queue.append(symbol)
         else:
-            if openers_closers[queue.pop()] != symbol:
-                return symbol_scores[symbol]
-    return 0
+            if OPENERS_CLOSERS[queue.pop()] != symbol:
+                return SyntaxScore(Type.ERROR, error_scores[symbol])
+
+    # no errors found, must be incomplete
+    incomplete_scores = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4,
+    }
+    value = 0
+    while queue:
+        symbol = queue.pop()
+        value *= 5
+        value += incomplete_scores[OPENERS_CLOSERS[symbol]]
+    return SyntaxScore(Type.INCOMPLETE, value)
 
 
 if __name__ == "__main__":
@@ -32,7 +62,15 @@ if __name__ == "__main__":
     puzzle = models.Puzzle(year=2021, day=10)
     data = transforms.lines(puzzle.input_data)
 
-    scores = 0
+    error_value_total = 0
+    incomplete_values = []
     for data_entry in data:
-        scores += get_error_score(data_entry)
-    puzzle.answer_a = scores
+        score = get_score(data_entry)
+        if score.type == Type.ERROR:
+            error_value_total += score.value
+        else:
+            incomplete_values.append(score.value)
+    # sum of all error scores
+    puzzle.answer_a = error_value_total
+    # median of all incomplete scores
+    puzzle.answer_b = sorted(incomplete_values)[len(incomplete_values) // 2]
--- day10/syntax_scoring_test.py
+++ day10/syntax_scoring_test.py
@@ -1,27 +1,37 @@
 import unittest
 
-from syntax_scoring import get_error_score
+from syntax_scoring import get_score, Type
 
 
 class SyntaxScoringTest(unittest.TestCase):
     def setUp(self):
         # navigation subsystem entry, error score
         self.data = [
-            ("[({(<(())[]>[[{[]{<()<>>", 0),
-            ("[(()[<>])]({[<{<<[]>>(", 0),
-            ("{([(<{}[<>[]}>{[]{[(<()>", 1197),
-            ("(((({<>}<{<{<>}{[]{[]{}", 0),
-            ("[[<[([]))<([[{}[[()]]]", 3),
-            ("[{[{({}]{}}([{[{{{}}([]", 57),
-            ("{<[[]]>}<{[{[{[]{()[[[]", 0),
-            ("[<(<(<(<{}))><([]([]()", 3),
-            ("<{([([[(<>()){}]>(<<{{", 25137),
-            ("<{([{{}}[<[[[<>{}]]]>[]]", 0),
+            ("[({(<(())[]>[[{[]{<()<>>", None, 288957),
+            ("[(()[<>])]({[<{<<[]>>(", None, 5566),
+            ("{([(<{}[<>[]}>{[]{[(<()>", 1197, None),
+            ("(((({<>}<{<{<>}{[]{[]{}", None, 1480781),
+            ("[[<[([]))<([[{}[[()]]]", 3, None),
+            ("[{[{({}]{}}([{[{{{}}([]", 57, None),
+            ("{<[[]]>}<{[{[{[]{()[[[]", None, 995444),
+            ("[<(<(<(<{}))><([]([]()", 3, None),
+            ("<{([([[(<>()){}]>(<<{{", 25137, None),
+            ("<{([{{}}[<[[[<>{}]]]>[]]", None, 294),
         ]
 
     def test_error_score(self):
-        for entry, score in self.data:
-            self.assertEqual(score, get_error_score(entry))
+        for entry, error_value, _ in self.data:
+            score = get_score(entry)
+            if error_value:
+                self.assertEqual(Type.ERROR, score.type)
+                self.assertEqual(error_value, score.value)
+
+    def test_incomplete_score(self):
+        for entry, _, incomplete_value in self.data:
+            score = get_score(entry)
+            if incomplete_value:
+                self.assertEqual(Type.INCOMPLETE, score.type)
+                self.assertEqual(incomplete_value, score.value)
 
 
 if __name__ == "__main__":
