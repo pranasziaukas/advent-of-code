9053d44baad6e968b4364fdb131fdca280aae18d
Pranas Ziaukas <pranas@balena.io>
2021-12-23 16:03:32 +0200

    solution: day21

--- day21/dice_game.py
+++ day21/dice_game.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from itertools import product
 
 
 @dataclass(frozen=True)
@@ -15,17 +16,17 @@ class Player:
         return Player(position, score)
 
 
-class Game:
-    def __init__(self, player_one: Player, player_two: Player) -> None:
-        # Next = playing next or catching up.
-        self.player_next = player_one
-        # Last = played last or already won.
-        self.player_last = player_two
+PlayerPair = (Player, Player)
 
+
+class PracticeGame:
+    def __init__(self, player_pair: PlayerPair = None) -> None:
+        self.players = list(player_pair) if player_pair else []
         self.dice = 0
         self.roll_count = 0
 
     def roll(self) -> int:
+        """Roll dice."""
         if self.dice == 100:
             self.dice = 1
         else:
@@ -33,22 +34,55 @@ class Game:
         self.roll_count += 1
         return self.dice
 
-    def take_turn(self) -> None:
-        dice_result = self.roll() + self.roll() + self.roll()
-        self.player_next += dice_result
-        self.player_next, self.player_last = self.player_last, self.player_next
+    def take_turn(self, player: Player) -> Player:
+        """Take one turn."""
+        dice_result = sum(self.roll() for _ in range(3))
+        return player + dice_result
 
     def play(self) -> None:
-        while self.player_last.score < 1000:
-            self.take_turn()
+        """Play a full game."""
+
+        # Player 1 starts first
+        n = 0
+        while all(player.score < 1000 for player in self.players):
+            self.players[n] = self.take_turn(self.players[n])
+            n = 1 - n
+
+
+class DiracGame:
+    def __init__(self) -> None:
+        self.seen_winning_universes: dict[PlayerPair, (int, int)] = {}
+
+    def winning_universes(self, player_pair: PlayerPair) -> (int, int):
+        """Count wins in different universes."""
+        x, y = 0, 0
+        try:
+            # Get a known result.
+            x, y = self.seen_winning_universes[player_pair]
+        except KeyError:
+            player_next, player_last = player_pair
+            if player_last.score >= 21:
+                y += 1
+            else:
+                for dice_rolls in product([1, 2, 3], repeat=3):
+                    new_player_pair = (player_last, player_next + sum(dice_rolls))
+                    dy, dx = self.winning_universes(new_player_pair)
+                    self.seen_winning_universes[new_player_pair] = (dy, dx)
+                    x += dx
+                    y += dy
+        finally:
+            return x, y
 
 
 if __name__ == "__main__":
     from aocd import models, transforms
 
     puzzle = models.Puzzle(year=2021, day=21)
-    position1, position2 = [int(line.split(" ")[-1]) - 1 for line in transforms.lines(puzzle.input_data)]
-    game = Game(Player(position1), Player(position2))
+    data = tuple(Player(int(line.split(" ")[-1]) - 1) for line in transforms.lines(puzzle.input_data))
 
-    game.play()
-    puzzle.answer_a = game.roll_count * game.player_next.score
+    practice_game = PracticeGame(data)
+    practice_game.play()
+    puzzle.answer_a = practice_game.roll_count * min(p.score for p in practice_game.players)
+
+    dirac_game = DiracGame()
+    puzzle.answer_b = max(dirac_game.winning_universes(data))
--- day21/dice_game_test.py
+++ day21/dice_game_test.py
@@ -1,23 +1,35 @@
 import unittest
 
-from dice_game import Player, Game
+from dice_game import DiracGame, Player, PracticeGame
 
 
 class FooTest(unittest.TestCase):
     def setUp(self):
-        self.game = Game(Player(3), Player(7))
+        self.player_pair = (Player(3), Player(7))
 
-    def test_one_turn(self):
-        self.game.take_turn()
-        self.assertEqual(Player(9, 10), self.game.player_last)
-        self.assertEqual(Player(7, 0), self.game.player_next)
-        self.assertEqual(3, self.game.roll_count)
+    def test_practice_one_turn_each(self):
+        game = PracticeGame()
 
-    def test_full_game(self):
-        self.game.play()
-        self.assertEqual(Player(9, 1000), self.game.player_last)
-        self.assertEqual(Player(2, 745), self.game.player_next)
-        self.assertEqual(993, self.game.roll_count)
+        player_one = self.player_pair[0]
+        player_one_after = game.take_turn(player_one)
+        self.assertEqual(Player(9, 10), player_one_after)
+        self.assertEqual(3, game.roll_count)
+
+        player_two = self.player_pair[1]
+        player_two_after = game.take_turn(player_two)
+        self.assertEqual(Player(2, 3), player_two_after)
+        self.assertEqual(6, game.roll_count)
+
+    def test_practice_full_game(self):
+        game = PracticeGame(self.player_pair)
+        game.play()
+        self.assertEqual(Player(9, 1000), game.players[0])
+        self.assertEqual(Player(2, 745), game.players[1])
+        self.assertEqual(993, game.roll_count)
+
+    def test_dirac_game(self):
+        game = DiracGame()
+        self.assertEqual((444356092776315, 341960390180808), game.winning_universes(self.player_pair))
 
 
 if __name__ == "__main__":
