9f7e49265a5b2e9be2bcb81cee39efd0073e9bed
Pranas Ziaukas <pranas@balena.io>
2022-12-02 22:20:51 +0200

    solution: day02

--- day02/rock_paper_scissors.py
+++ day02/rock_paper_scissors.py
@@ -7,7 +7,11 @@ class Shape(IntEnum):
     SCISSORS = 3
 
 
-def parse(raw_data: str) -> [(Shape, Shape)]:
+def parse(raw_data: str) -> [(str, str)]:
+    return [tuple(raw_shapes.split()) for raw_shapes in raw_data.splitlines()]
+
+
+def find_shapes_simple(shape_codes: (str, str)) -> (Shape, Shape):
     shape_by_code = {
         "A": Shape.ROCK,
         "X": Shape.ROCK,
@@ -16,7 +20,27 @@ def parse(raw_data: str) -> [(Shape, Shape)]:
         "C": Shape.SCISSORS,
         "Z": Shape.SCISSORS,
     }
-    return [tuple(shape_by_code[x] for x in raw_shapes.split()) for raw_shapes in raw_data.splitlines()]
+    return tuple(shape_by_code[x] for x in shape_codes)
+
+
+def find_shapes_advanced(shape_codes: (str, str)) -> (Shape, Shape):
+    shape_by_code = {
+        "A": Shape.ROCK,
+        "B": Shape.PAPER,
+        "C": Shape.SCISSORS,
+    }
+    that_code, this_code = shape_codes
+    that_shape = shape_by_code[that_code]
+
+    shift_by_code = {
+        "X": -1,
+        "Y": 0,
+        "Z": 1,
+    }
+    this_shift = shift_by_code[this_code]
+    this_shape = Shape((that_shape + this_shift - 1) % 3 + 1)
+
+    return that_shape, this_shape
 
 
 def score(that_shape: Shape, this_shape: Shape) -> int:
@@ -36,12 +60,14 @@ def score(that_shape: Shape, this_shape: Shape) -> int:
 
 
 if __name__ == "__main__":
-    from aocd import models, transforms
+    from aocd import models
 
     puzzle = models.Puzzle(year=2022, day=2)
 
     data_demo = parse(puzzle.example_data)
-    assert sum(score(*x) for x in data_demo) == 15
+    assert sum(score(*find_shapes_simple(x)) for x in data_demo) == 15
+    assert sum(score(*find_shapes_advanced(x)) for x in data_demo) == 12
 
     data = parse(puzzle.input_data)
-    puzzle.answer_a = sum(score(*x) for x in data)
+    puzzle.answer_a = sum(score(*find_shapes_simple(x)) for x in data)
+    puzzle.answer_b = sum(score(*find_shapes_advanced(x)) for x in data)
--- day02/rock_paper_scissors_test.py
+++ day02/rock_paper_scissors_test.py
@@ -1,9 +1,9 @@
 import unittest
 
-from rock_paper_scissors import Shape, score
+from rock_paper_scissors import Shape, score, find_shapes_simple, find_shapes_advanced
 
 
-class FooTest(unittest.TestCase):
+class RockPaperScissorsTest(unittest.TestCase):
     def test_score_tie(self):
         self.assertEqual(4, score(Shape.ROCK, Shape.ROCK))
         self.assertEqual(6, score(Shape.SCISSORS, Shape.SCISSORS))
@@ -15,6 +15,14 @@ class FooTest(unittest.TestCase):
     def test_score_win(self):
         self.assertEqual(7, score(Shape.SCISSORS, Shape.ROCK))
         self.assertEqual(8, score(Shape.ROCK, Shape.PAPER))
+
+    def test_find_shapes_simple(self):
+        self.assertEqual((Shape.ROCK, Shape.ROCK), find_shapes_simple(("A", "X")))
+        self.assertEqual((Shape.PAPER, Shape.SCISSORS), find_shapes_simple(("B", "Z")))
+
+    def test_find_shapes_advanced(self):
+        self.assertEqual((Shape.ROCK, Shape.SCISSORS), find_shapes_advanced(("A", "X")))
+        self.assertEqual((Shape.PAPER, Shape.SCISSORS), find_shapes_advanced(("B", "Z")))
 
 
 if __name__ == "__main__":
