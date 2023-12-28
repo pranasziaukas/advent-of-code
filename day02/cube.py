67a7b672caf37667a1871810c78800f9d5d4a113
Pranas Ziaukas <pranas@balena.io>
2023-12-28 18:26:06 +0200

    solution: day02

--- day02/cube.py
+++ day02/cube.py
@@ -1,29 +1,22 @@
-from dataclasses import dataclass
-
-
-@dataclass(frozen=True)
 class Game:
-    red: int
-    green: int
-    blue: int
+    def __init__(self, line: str) -> None:
+        gid, turns = line.split(': ')
+        self.id = int(gid[5:])
+        self.max_red = self.max_green = self.max_blue = 0
 
-    def has(self, color: str) -> int:
-        return self.__dict__[color]
+        for turn in turns.split('; '):
+            for n, color in map(str.split, turn.split(', ')):
+                n = int(n)
 
+                if color == 'red':
+                    self.max_red = max(n, self.max_red)
+                elif color == 'green':
+                    self.max_green = max(n, self.max_green)
+                else:
+                    self.max_blue = max(n, self.max_blue)
 
-GAME = Game(12, 13, 14)
-
-
-def is_possible(line: str) -> int:
-    title, facts_raw = line.split(": ")
-    facts = facts_raw.split("; ")
-    for fact in facts:
-        observations = fact.split(", ")
-        for observation in observations:
-            value, color = observation.split(" ")
-            if GAME.has(color) < int(value):
-                return 0
-    return int(title.split(" ")[1])
+    def is_allowed(self) -> bool:
+        return self.max_red <= 12 and self.max_green <= 13 and self.max_blue <= 14
 
 
 if __name__ == "__main__":
@@ -32,7 +25,9 @@ if __name__ == "__main__":
     puzzle = models.Puzzle(year=2023, day=2)
 
     data_demo = transforms.lines(puzzle.example_data)
-    assert sum(is_possible(x) for x in data_demo) == 8
+    assert sum(Game(x).id for x in data_demo if Game(x).is_allowed()) == 8
 
     data = transforms.lines(puzzle.input_data)
-    puzzle.answer_a = sum(is_possible(x) for x in data)
+    games = [Game(x) for x in data]
+    puzzle.answer_a = sum(game.id for game in games if game.is_allowed())
+    puzzle.answer_b = sum(game.max_red * game.max_green * game.max_blue for game in games)
--- day02/cube_test.py
+++ day02/cube_test.py
@@ -1,21 +1,26 @@
 import unittest
 
-from cube import is_possible
+from cube import Game
 
 
 class FooTest(unittest.TestCase):
     def setUp(self):
         self.data = [
-            ('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', 1),
-            ('Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue', 2),
-            ('Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', 0),
-            ('Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', 0),
-            ('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green', 5),
+            ('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green', (1, 4, 2, 6)),
+            ('Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue', (2, 1, 3, 4)),
+            ('Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red', (3, 20, 13, 6)),
+            ('Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red', (4, 14, 3, 15)),
+            ('Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green', (5, 6, 3, 2)),
         ]
 
-    def test_cube(self):
+    def test_game_parse(self):
         for line, expected in self.data:
-            self.assertEqual(is_possible(line), expected)
+            game_id, max_red, max_green, max_blue = expected
+            game = Game(line)
+            self.assertEqual(game.id, game_id)
+            self.assertEqual(game.max_red, max_red)
+            self.assertEqual(game.max_green, max_green)
+            self.assertEqual(game.max_blue, max_blue)
 
 
 if __name__ == "__main__":
